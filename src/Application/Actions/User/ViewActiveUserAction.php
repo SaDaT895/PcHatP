db390964b4edd8c69b8c1cfd763d1406065a0284
Sadat Ahmad <sadatahmadofficial@gmail.com>
2025-03-15 19:23:12 +0100

    refactor(api): update return data for view-active-user action

--- src/Application/Actions/User/ViewActiveUserAction.php
+++ src/Application/Actions/User/ViewActiveUserAction.php
@@ -7,6 +7,7 @@ namespace App\Application\Actions\User;
 use App\Application\Actions\Action;
 use Slim\Exception\HttpBadRequestException;
 use Slim\Psr7\Response;
+use App\Domain\User\User;
 
 
 class ViewActiveUserAction extends Action
@@ -14,6 +15,6 @@ class ViewActiveUserAction extends Action
     protected function action(): Response
     {
         if (!isset($_SESSION['activeUser'])) return $this->respondWithData('No user set in session. Choose or create one to get started');
-        return $this->respondWithData('Active user:' . $_SESSION['activeUser']);
+        return $this->respondWithData(User::find($_SESSION['activeUser']));
     }
 }
